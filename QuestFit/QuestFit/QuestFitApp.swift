aefe0dfae1374eb3846c4a507f913e26766b044a
Dino Miguel Bunag <92753719+deenoe@users.noreply.github.com>
2024-02-15 21:59:29 -0800

    Add files via upload

---
+++ QuestFit/QuestFit/QuestFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestFitApp.swift
+//  QuestFit
+//
+//  Created by Miguel Bunag on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
