a037380bc4da4ffb08a2e4d621568405091d1711
Dino Miguel Bunag <mbunag06@gmail.com>
2024-02-23 16:42:55 -0800

    homepage styling

--- QuestFit/QuestFit/QuestFit/DailyQuestsView.swift
+++ QuestFit/QuestFit/QuestFit/DailyQuestsView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct DailyQuestsView: View {
     var body: some View {
-        Text("Daily Quests Here")
+        let mainColor = Color(red: 0/255, green: 55/255, blue: 0/255)
+        let accentColor = Color(red: 152/255, green: 158/255, blue: 143/255)
+        
+        ZStack{
+            Color(accentColor)
+                .ignoresSafeArea()
+            Text("Daily Quests Here")
+        }
     }
 }
 
